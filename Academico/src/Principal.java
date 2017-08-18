aed6b81e9c92d22d255c329186355f45d370aa5f
RA21708452 <RA21708452@AN0700114077504.dinfor.uniceub>
2017-08-18 19:41:27 -0300

    oi

--- Academico/src/Principal.java
+++ Academico/src/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 		Aluno a1 = new Aluno ("Alexandra", "20708050", 21, "Medicina", "Manha", "Feminina");
 		Disciplina d1 = new Disciplina ("fisionomia", "Medicina", 40);
 		
-		Aluno a2 = new Aluno ("Josue", "21708452", 18, "Ciençia da Computacao", "Noturno", "Masculino");
+		Aluno a2 = new Aluno ("Josue", "21708452", 18, "Ciencia da Computacao", "Noturno", "Masculino");
 		Disciplina d2 = new Disciplina ("LP 1", "CC", 40);
 		
 		System.out.println(a1.imprimirAluno());
