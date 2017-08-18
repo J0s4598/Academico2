9496246f6f4da126deadae18092f27dfbef2ae38
J0s4598 <31115712+J0s4598@users.noreply.github.com>
2017-08-17 22:21:45 -0300

    Mais um
    Coloquei um system a mais

--- Academico/src/Principal.java
+++ Academico/src/Principal.java
@@ -4,10 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Aluno a1 = new Aluno ("Alexandra", "20708050", 21, "Medicina", "Manh„", "Feminina");
+		Aluno a1 = new Aluno ("Alexandra", "20708050", 21, "Medicina", "Manh√£", "Feminina");
 		Disciplina d1 = new Disciplina ("fisionomia", "Medicina", 40);
 		
-		Aluno a2 = new Aluno ("JosuÈ", "21708452", 18, "CienÁia da ComputaÁ„o", "Noturno", "Masculino");
+		Aluno a2 = new Aluno ("Josu√©", "21708452", 18, "Cien√ßia da Computa√ß√£o", "Noturno", "Masculino");
 		Disciplina d2 = new Disciplina ("LP 1", "CC", 40);
 		
 		System.out.println(a1.imprimirAluno());
@@ -15,7 +15,7 @@ public class Principal {
 		System.out.println(a2.imprimirAluno());
 		System.out.println(d2);
 		
-		
+		System.out.println(a1.getNome());
 		System.out.println(a1.getIdade());
 		
 		
