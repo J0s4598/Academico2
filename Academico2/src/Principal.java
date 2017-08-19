ee5f7efa5c10ea0eea80be2d8d4b79bcbef477b3
RA21708452 <RA21708452@AN0700114077504.dinfor.uniceub>
2017-08-18 22:01:19 -0300

    ArrayList foi adicionado

--- Academico2/src/Principal.java
+++ Academico2/src/Principal.java
@@ -10,14 +10,24 @@ public class Principal {
 		Aluno a1 = new Aluno ("Alexandra", "20708050", "Feminina", LocalDate.of(1995, Month.JUNE, 21));
 		Disciplina d1 = new Disciplina ("LP 1", "CC", 40);	
 		Professor p1 = new Professor ("Marco", "1643297852", "Pos-Graduação", "70292998172");
-		Turma t1 = new Turma ("01", p1.getNome(), d1.getNome());
-		
+		Turma t1 = new Turma ("B", p1.getNome(), d1.getNome());
+	
+		//Adicionar a1 em t1
+		t1.adicionarAluno(a1);
+				
+		//Imprimir imformaçao basica
 		System.out.println(a1);
 		System.out.println(d1);
 		System.out.println(p1);
 		System.out.println(t1);
 		
+		System.out.println(t1.getAluno().get(0).getNome());
 		
+		
+		Aluno a2 = new Aluno ("Josue", "21708452", "Masculina", LocalDate.of(1999, Month.MAY, 25));
+		t1.adicionarAluno(a2);
+		
+		System.out.println(t1.getAluno().get(1).getNome());
 		
 
 	}
--- Academico2/src/Turma.java
+++ Academico2/src/Turma.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 
 public class Turma {
 	private String codigo;
 	private String professor;
 	private String disciplina;
+	private ArrayList<Aluno> aluno;
 	
 	//Metodo construtor
 	public Turma(String codigo, String professor, String disciplina) {
@@ -10,6 +12,7 @@ public class Turma {
 		this.codigo = codigo;
 		this.professor = professor;
 		this.disciplina = disciplina;
+		this.aluno = new ArrayList<Aluno>();
 	}
 
 	//toString
@@ -41,6 +44,21 @@ public class Turma {
 	public void setDisciplina(String disciplina) {
 		this.disciplina = disciplina;
 	}
+	
+	public ArrayList<Aluno> getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(ArrayList<Aluno> aluno) {
+		this.aluno = aluno;
+	}
+	
+	
+	//Metodo que adiciona 1 aluno na turma
+	public void adicionarAluno(Aluno aluno){
+		this.aluno.add(aluno);
+	}
+
 	
 	
 	
